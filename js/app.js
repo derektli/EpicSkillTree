9abf81c3101a9ebc5924f01694f56665c3a0d549
RodEsp <respinosa@foundops.com>
2013-02-02 12:27:16 -0500

    Hopefully this will fix mhacks rhcloud hosting.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('EpicSkillTree', ['EpicSkillTree.filters', 'EpicSkillTree.services', 'EpicSkillTree.directives']).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/initial', {templateUrl: 'partials/initial.html', controller: InitialController});
+        $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: AboutController});
+        $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactController});
+        $routeProvider.when('/treemaker', {templateUrl: 'partials/treemaker.html', controller: TreeMakerController});
+        $routeProvider.otherwise({redirectTo: '/initial'});
+    }]);
---
+++ js/controllers.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* Controllers */
+function InitialController() {
+}
+InitialController.$inject = [];
+
+function AboutController() {
+}
+AboutController.$inject = [];
+
+function ContactController() {
+}
+ContactController.$inject = [];
+
+function TreeMakerController() {
+
+    var addNodePopupTemplate = $("#addNodePopup"),
+//
+//            $('<div class="popupTemplate" id="addNodePopup">' +
+//            '<form id="addNodeForm" action="javascript:$(\"addNodeForm\").submit();">' +
+//                '<fieldset>' +
+//                    '<label>Title:</label><input type="text" name="title"/>' +
+//                    '<label>Link:</label><input type="text" name="link"/>' +
+//                    '<label>Description:</label><textarea name="desc"></textarea>' +
+//                    '<br/>' +
+//                    '<input type="submit" value="Save Node" class="submit btn btn-primary saveButton"/>' +
+//                '</fieldset>' +
+//            '</form>' +
+//            '</div>'),
+
+        addNodePopup = $(".addNode").popup({
+            content : addNodePopupTemplate
+        }).data('popup'),
+
+        createNode = function (node) {
+
+        };
+
+    $('#addNodeForm').submit(function (e) {
+        e.preventDefault();
+        var title = $("input[name=title]")[1].value,
+            link = $("input[name=link]")[1].value,
+            description = $("textarea[name=desc]")[1].value,
+            node = {title: title, link: link, desc: description},
+            newNode = createNode(node);
+        addNodePopup.close();
+    });
+}
+TreeMakerController.$inject = [];
