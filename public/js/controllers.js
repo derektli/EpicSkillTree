3284ea04a38eb96dd7bbed996d30be7337e6b85e
RodEsp <respinosa@foundops.com>
2013-02-02 09:34:43 -0500

    Setup angularjs.

---
+++ public/js/controllers.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/* Controllers */
+
+function InitialController() {
+}
+InitialController.$inject = [];
+
+function AboutController() {
+}
+AboutController.$inject = [];
+
+function ContactController() {
+}
+ContactController.$inject = [];
+
+function TreeMakerController() {
+//    var addNodePopup = $(".addNode").popup({
+//        content : $('#addNodePopup')
+//    }).data('popup');
+//
+//    var closeAddNodePopup = function () {
+//        addNodePopup.close();
+//    };
+//
+//    $('#addNodeForm').submit(function (e) {
+//        console.log("bleh");
+//        e.preventDefault();
+//        return false;
+//    });
+}
+TreeMakerController.$inject = [];
