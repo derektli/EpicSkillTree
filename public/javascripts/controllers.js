16a6deacb8080826caa336f4e2e4df683e1065a2
RodEsp <respinosa@foundops.com>
2013-02-03 00:54:43 -0500

    Trees now finished. Need to add JSON/HTML parser and nodePopup functionality.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -15,34 +15,79 @@ ContactController.$inject = [];
 
 function TreeMakerController($scope) {
 
-    var addNodePopupTemplate = $("#addNodePopup"),
-//
-//            $('<div class="popupTemplate" id="addNodePopup">' +
-//            '<form id="addNodeForm" action="javascript:$(\"addNodeForm\").submit();">' +
-//                '<fieldset>' +
-//                    '<label>Title:</label><input type="text" name="title"/>' +
-//                    '<label>Link:</label><input type="text" name="link"/>' +
-//                    '<label>Description:</label><textarea name="desc"></textarea>' +
-//                    '<br/>' +
-//                    '<input type="submit" value="Save Node" class="submit btn btn-primary saveButton"/>' +
-//                '</fieldset>' +
-//            '</form>' +
-//            '</div>'),
+    $('<ul>' +
+        '<li>Root' +
+            '<ul>' +
+                '<li>One</li>' +
+                '<li>Two' +
+                    '<ul>' +
+                        '<li>Two One</li>' +
+                        '<li>Two Two</li>' +
+                    '</ul>' +
+                '</li>' +
+            '</ul>' +
+        '</li>' +
+    '</ul>').jOrgChart({chartElement: $("#exampleTree")});
 
-        addNodePopup = $(".addNode").popup({
-            content : addNodePopupTemplate
-        }).data('popup');
+    var tree = {}, node = {};
 
-    $scope.addNode = function () {
-        var title = $("input[name=title]")[1].value,
-            link = $("input[name=link]")[1].value,
-            description = $("textarea[name=desc]")[1].value,
-            node = {title: title, link: link, desc: description};
-        addNodePopup.close();
-    };
+        //Accepts either a JSON object or a <ul> and parses it into a <ul> or a JSON object respectively.
+    var nodeJSONParser = function (node, dataFormat) {
+            if(dataFormat === "HTML") {
 
-    $('#addNodeForm').submit(function (e) {
-        e.preventDefault();
+            } else if (dataFormat === "JSON") {
+
+            }
+        },
+        treeJSONParser = function (tree, dataFormat) {
+            if(dataFormat === "HTML") {
+
+            } else if (dataFormat === "JSON") {
+
+            }
+        },
+        createTree = function (treeName) {
+            var newTree = {
+                "node": {
+                    "name": treeName,
+                    "links": [],
+                    "desc": null,
+                    "parent": null,
+                    "children": [],
+                    "treeName": treeName
+                }
+            };
+            return newTree;
+        },
+        createNode = function (name, links, desc, parent, children, treeName) {
+
+        },
+        //Accepts a UL, turns it into a tree, and attaches it to the DOM.
+        attachTree = function (tree, elementToAppendTo) {
+            $(tree).jOrgChart({chartElement: $(elementToAppendTo)})
+        };
+
+    $("#startTreeButton").on("click", function () {
+        console.log("Creating Tree...");
+        var newTree = createTree($("#treeNameTextBox").val()),
+            newTreeUL = $("<ul name="+newTree.node.treeName+">" +
+                            "<li>" +
+                                newTree.node.treeName +
+                            "</li>" +
+                        "</ul>");
+        attachTree(newTreeUL, $("#newTree"));
+//        attachTree(treeJSONParser(newTree, "HTML"), $("#newTree"));
+        $(this.parentElement).css("display", "none");
+    });
+    $(document).on("click", ".node", function (e) {
+        $("#nodePopup").bPopup({
+            onOpen: function () {
+                //Load node values
+            },
+            onClose: function () {
+                //Clear input values
+            }
+        })
     });
 }
 TreeMakerController.$inject = [];
