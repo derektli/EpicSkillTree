d1420a3a677b80151b915d337a7b13ea15dcbf68
formatjam <formatjam@gmail.com>
2013-02-03 03:12:49 -0500

    added gettree controller

--- app/controllers/treecontrol_controller.rb
+++ app/controllers/treecontrol_controller.rb
@@ -30,6 +30,44 @@ class TreecontrolController < ApplicationController
 	
   end
 
+  
+  def node_to_json(s_node)
+      children = []
+      for x in Edge.all
+          if x.parent == s_node.name
+                #children += [{:name => x.child}]
+                children += [node_to_json(Node.find_by_name(x.child))]
+          end
+      end
+      
+      return_json = {:name => s_node.name, :link => s_node.link, :description => s_node.description, :children => children}
+      return return_json
+  end
+  
+  
   def gettree
+      tree_name = params[:treename]
+      t = Tree.find_by_name(tree_name)
+      
+      return_json = {:msg => "fail"}
+      
+      if t!=nil
+          
+          root_node = Node.find_by_name(tree_name)
+          root_node = {:node => node_to_json(root_node)}
+          return_json = {:msg => "success", :tree => root_node}
+          
+    #for x in t.edges 
+    #if x.parents == ""
+    #x.child 
+    #Node.find_by_name
+
+      end
+      
+      
+      respond_to do |format|
+          format.json { render :json => return_json }
+      end
+      
   end
 end
